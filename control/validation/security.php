377c50e5e457b22c3164b09d2bde044cddef9558
amirpashaie <mr.amirpashaie@gmail.com>
2017-06-25 11:18:16 +0430

    security class

---
+++ control/validation/security.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: amir
+ * Date: 5/20/2017
+ * Time: 8:59 AM
+ */
+
+class security{
+
+    public function check_access(){}
+
+    public function check_sql(){}
+
+}
