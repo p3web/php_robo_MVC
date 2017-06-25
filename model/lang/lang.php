f0aef30ec77f410fb1686c1976be5b29e857399a
amirpashaie <mr.amirpashaie@gmail.com>
2017-06-25 11:16:29 +0430

    language class for messages

---
+++ model/lang/lang.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: amir
+ * Date: 5/21/2017
+ * Time: 8:08 AM
+ */
+namespace model\lang;
+
+class lang{
+
+    public static $register_data = 'اطلاعات با موفقیت ثبت شد';
+    public static $success       = 'اطلاعات با موفقیت ثبت شد';
+    public static $failed        = 'عملیات ناموفق بود';
+    public static $invalid_data  = 'اطلاعات وارد شده معتبر نمی باشد';
+    public static $is_not_login  = 'ایمیل یا رمزعبور معتبر نمی باشد';
+    public static $last_registered_data = 'قبلا این اطلاعات ثبت شده است';
+    public static $is_not_set_profile   = 'در ثبت اطلاعات شما مشکل پیش آمده است لطفا دورباره تلاش کنید';
+    public static $not_data_here = "داده ای وجود ندارد.";
+    public static $error   = 'خطا!';
+    public static $message = 'پیغام';
+
+}
